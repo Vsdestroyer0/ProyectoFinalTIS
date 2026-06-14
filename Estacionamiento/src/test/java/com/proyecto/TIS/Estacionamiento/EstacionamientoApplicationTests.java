91ad0b76a178a593425202c3742b8ad952f52b92
Vsdestroyer0 <ggmendoza2005@gmail.com>
2026-06-14 17:46:40 -0600

    feat: Configuraciones de Spring de cada servicio
    Se ingresaron las configuraciones básicas de spring boot en el proyecto, además de configurar las dependencias de JWT para los usuarios

---
+++ Estacionamiento/src/test/java/com/proyecto/TIS/Estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.TIS.Estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ users/src/test/java/com/proyecto/TIS/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.TIS.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
