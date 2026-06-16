ad502f2afe22be75c5388b072f4964e7aaa450ad
Emir <turumbar451@hotmail.com>
2026-06-16 03:55:12 -0600

    vehiculoController para endpoints

--- Vehiculos/src/main/java/com/proyecto/TIS/Vehiculos/controller/VehiculosController.java
+++ Vehiculos/src/main/java/com/proyecto/TIS/Vehiculos/controller/VehiculosController.java
@@ -1,9 +1,26 @@
 package com.proyecto.TIS.Vehiculos.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.proyecto.TIS.Vehiculos.entity.VehiculosEntity;
+import com.proyecto.TIS.Vehiculos.service.VehiculosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/vehiculos")
+@RequestMapping("/vehiculos")
 public class VehiculosController {
+
+    @Autowired
+    private VehiculosService service;
+
+    @PostMapping("/guardar")
+    public ResponseEntity<?> guardar(@RequestBody VehiculosEntity vehiculo) {
+        try {
+            VehiculosEntity nuevoVehiculo = service.guardarVehiculo(vehiculo);
+            return ResponseEntity.ok(nuevoVehiculo);
+        } catch (Exception e) {
+            // mensaje de error questa en el service
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
