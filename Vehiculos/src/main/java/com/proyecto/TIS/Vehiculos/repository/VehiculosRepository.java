29cd6d151aa38cc0c8726856e4cf4faee8ab049a
Emir <turumbar451@hotmail.com>
2026-06-16 03:36:33 -0600

    vehiculosRepository con consultas JPA

--- Vehiculos/src/main/java/com/proyecto/TIS/Vehiculos/repository/VehiculosRepository.java
+++ Vehiculos/src/main/java/com/proyecto/TIS/Vehiculos/repository/VehiculosRepository.java
@@ -1,7 +1,20 @@
 package com.proyecto.TIS.Vehiculos.repository;
 
+import com.proyecto.TIS.Vehiculos.entity.VehiculosEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface VehiculosRepository {
+public interface VehiculosRepository extends JpaRepository<VehiculosEntity, Integer> {
+
+    //todos los vehiculos de un usuario especifico
+    List<VehiculosEntity> findByIdUsuario(Integer idUsuario);
+
+    
+    //contar cuantos vehiculs activos tiene un usuarui
+    long countByIdUsuarioAndEstatusTrue(Integer idUsuario);
+
+    // validad si la placa ya existe
+    boolean existsByPlaca(String placa);
 }
